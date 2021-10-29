3a2792f958bbde064a2df2f67a24215cfa2cad69
vrudyk <vruduk@solvd.com>
2021-10-29 10:06:09 +0300

    create project

---
+++ src/main/java/com/solvd/Main.java
@@ -0,0 +1,12 @@
+package com.solvd;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+    }
+}
